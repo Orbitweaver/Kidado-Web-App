904366d200d4e681489eee348e94c0a688b07eec
Abbas Bhanpura wala <99265597+Abbas-Dev-786@users.noreply.github.com>
2026-02-27 21:44:23 +0530

    fix: tab redirection issue

--- src/components/auth/auth-tabs.tsx
+++ src/components/auth/auth-tabs.tsx
@@ -13,25 +13,26 @@ const AuthTabs = () => {
     navigate(path);
   };
 
+  const currentTab = currentRoute.pathname.includes("/sign-in")
+    ? "sign-in"
+    : "sign-up";
+
   return (
     <Tabs
-      defaultValue={
-        currentRoute.pathname.includes("/sign-in") ? "sign-in" : "sign-up"
-      }
+      value={currentTab}
+      onValueChange={(value) => handleTabChange(`/${value}`)}
       className="w-full"
     >
       <TabsList className="w-full mb-6 bg-accent/60 text-[16px] font-medium">
         <TabsTrigger
           value="sign-up"
-          className="data-[state=active]:bg-primary data-[state=active]:text-white text-[16px]font-medium"
-          onClick={() => handleTabChange("/sign-up")}
+          className="data-[state=active]:bg-primary data-[state=active]:text-white text-[16px] font-medium"
         >
           Sign Up
         </TabsTrigger>
         <TabsTrigger
           value="sign-in"
-          className="data-[state=active]:bg-primary data-[state=active]:text-white text-[16px]font-medium"
-          onClick={() => handleTabChange("/sign-in")}
+          className="data-[state=active]:bg-primary data-[state=active]:text-white text-[16px] font-medium"
         >
           Sign In
         </TabsTrigger>
