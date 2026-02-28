02db2e3e435ef1d9979292141a23f45930ee5ce0
Abbas Bhanpura wala <99265597+Abbas-Dev-786@users.noreply.github.com>
2026-02-28 10:23:19 +0530

    fix: google btn width

--- src/components/auth/google-auth-btn.tsx
+++ src/components/auth/google-auth-btn.tsx
@@ -1,12 +1,22 @@
 import { useGoogleAuth } from "@/hooks/auth/useGoogleAuth";
 import { cn } from "@/lib/utils";
 import { GoogleLogin } from "@react-oauth/google";
+import { useEffect, useRef, useState } from "react";
 
 const GoogleAuthBtn = ({ disabled = false }: { disabled?: boolean }) => {
   const { mutate: loginWithGoogle, isPending } = useGoogleAuth();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(400);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setWidth(containerRef.current.offsetWidth);
+    }
+  }, []);
 
   return (
     <div
+      ref={containerRef}
       className={cn(
         "w-full",
         (disabled || isPending) && " pointer-events-none opacity-50",
@@ -20,6 +30,7 @@ const GoogleAuthBtn = ({ disabled = false }: { disabled?: boolean }) => {
         useOneTap={false}
         text="signin_with"
         logo_alignment="center"
+        width={width}
         containerProps={{
           className:
             "w-full rounded-md text-sm font-medium border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
